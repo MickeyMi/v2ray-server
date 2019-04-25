2a4211cf6a8c51464d45573843f64348ca974f81
MickeyMi <31444844+MickeyMi@users.noreply.github.com>
2019-04-25 20:59:02 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -13,30 +13,9 @@ yum -y install git unzip wget ntpdate
 
 ntpdate pool.ntp.org
 
-local=$(cd `dirname $0`; pwd)
-v2ray=/mickey/v2ray-server
-
-
-echo local-path $local
-echo v2ray-path $v2ray
-
-if [ ! -d $v2ray ];then
-    git clone https://github.com/MickeyMi/v2ray-server.git -l $v2ray
-else
-    git pull https://github.com/MickeyMi/v2ray-server.git -l $v2ray
-fi
-
 bash <(curl -L -s https://install.direct/go.sh)
 
-mv -f $v2ray/config.json /etc/v2ray/config.json
-mv -f $v2ray/v2ray.sh $local/v2ray.sh
-
-chmod 777 $local/v2ray.sh
+https://raw.githubusercontent.com/MickeyMi/v2ray-server/master/config.json -O /etc/v2ray/config.json
 
 service v2ray stop
 service v2ray start
-testtesttesttesttesttesttesttesttesttesttesttesttesttest
-
-
-
-
