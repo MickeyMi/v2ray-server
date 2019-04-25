acf34f41cf72f10745afc3cbcf1c620734980735
MickeyMi <31444844+MickeyMi@users.noreply.github.com>
2019-04-25 18:46:49 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,7 +1,13 @@
 yum -y install git unzip wget
 
+local=$(cd `dirname $0`; pwd)
 path="/mickey/shell"
 v2ray=$path"/v2ray-server"
+
+
+echo local-path $local
+echo shell-path $path
+echo v2ray-path $v2ray
 
 mkdir -p $path
 mkdir -p $v2ray
@@ -13,7 +19,7 @@ wget https://install.direct/go.sh -O $path/go.sh
 bash $path/go.sh -f
 
 mv -f $v2ray/config.json /etc/v2ray/config.json
-
-mv -f $v2ray/v2ray.sh $pwd/v2ray.sh
+mv -f $v2ray/v2ray.sh $local/v2ray.sh
 
 rm -rf $v2ray
+rm -rf $path
